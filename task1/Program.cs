59db4b85df21238bc1a2035ed48fda4dec08792f
EkaterinaAsanova <u-kat@mail.ru>
2022-12-02 20:31:18 +0500

    Число выводит в квадрат

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Программа,которая на вход принимает число и выдает его квадрат (число умноженное само на себя)
+
+//string text = "";
+//text = Console.ReadLine(); // number=int, Console.ReadLine() - string    Перевод текстового форматат реадлайна в числовой, чтобы применять формулы
+
+int number = 0;
+number=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Result: " + number * number);
