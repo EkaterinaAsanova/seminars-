4a1773da41811b55f7096c1bbe5bb11804a94240
EkaterinaAsanova <u-kat@mail.ru>
2022-12-02 20:53:29 +0500

    Является ли первое число квадратом второго

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа на вход принимает 2 числа и проверяет, является ли первое число квадратом второго
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1==number2*number2)
+{
+    Console.WriteLine ("Yes");
+}
+else
+{
+    Console.WriteLine ("No");
+}
