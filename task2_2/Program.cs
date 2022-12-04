a77af82500af6e4b83b093cdfffc32319a01df05
EkaterinaAsanova <u-kat@mail.ru>
2022-12-04 22:28:58 +0500

    Из 3-х значного числа убирает вторую цифру

---
+++ task2_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+// 456->46
+// 782->72
+// 918 ->98
+
+Random random = new Random();
+int number = 0;
+number = random.Next(100, 1000);
+Console.WriteLine(number);
+int a = number /100;
+int b = number % 10;
+
+Console.WriteLine("Двухзначное число:" +a+b);
