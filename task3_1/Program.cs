acdeceac6eb558738cec181801991a415c694ada
EkaterinaAsanova <u-kat@mail.ru>
2022-12-06 13:04:49 +0500

    Проверка на ввод числа или строки

---
+++ task3_1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Проверка вводимого значения пользователя на ввод числа или строки
+
+string text = Console.ReadLine();
+int number;
+bool success = int.TryParse(text, out number);
+Console.WriteLine(success + " " + number);
