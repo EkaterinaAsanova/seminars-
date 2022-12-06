2d3bbae1aed2ed7ebb49a0c9cc044b8157e6a95b
EkaterinaAsanova <u-kat@mail.ru>
2022-12-06 16:44:22 +0500

    По координатам 2х точек определяет расстояния в 2D пространстве. Массив

---
+++ task3_8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты 2х точек и находит расстояние в 2D пространстве.
+// А(3,6);В(2,1)  =  5.09
+// А(7,-5);В(1,-1)  =  7.21
+// с использованием массива! 
+
+double ab=0;
+double [] point = new double[4];
+for (int i=0; i< point.Length; i++)
+point[i]=Convert.ToDouble(Console.ReadLine());
+
+ab = Math.Sqrt((point[0]-point[2])*(point[0]-point[2]) + (point[1]-point[3])*(point[1]-point[3]));
+Console.WriteLine(ab); 
