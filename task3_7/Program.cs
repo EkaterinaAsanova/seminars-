4d50c80f6044cd52d3e78ec2c280ad1038f6e935
EkaterinaAsanova <u-kat@mail.ru>
2022-12-06 16:23:35 +0500

    По координатам 2х точек находит расстояние в 2D пространстве

---
+++ task3_7/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты 2х точек и находит расстояние в 2D пространстве.
+// А(3,6);В(2,1)  =  5.09
+// А(7,-5);В(1,-1)  =  7.21
+
+double x=0, y=0,x1=0,y1=0, ab=0;
+x = Convert.ToDouble(Console.ReadLine());
+y = Convert.ToDouble(Console.ReadLine());
+x1 = Convert.ToDouble(Console.ReadLine());
+y1 = Convert.ToDouble(Console.ReadLine());
+
+ab = Math.Sqrt((x1-x)*(x1-x) + (y1-y)*(y1-y));
+Console.WriteLine(ab); 
