458147743a5a579fb163ca5584fd851e932362a4
EkaterinaAsanova <u-kat@mail.ru>
2022-12-06 15:13:19 +0500

    Математический метод к задаче №13. Вывод 3 цифры

---
+++ task3_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Математический метод к задаче №13. Вывод 3 цифры если ее нет, то сообщить об ошибке
+
+int number= Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if (number>999)
+    while (number>999)
+    number =number / 10;
+if (number < 1000 && number > 99)
+result = number % 10;
+//125%10 = 5
+// 142124 /10 = 14212
+// 14212 / 10 = 1421
+// 1421 / 10 = 142
+// 142 % 10 =2
+Console.WriteLine (result);
