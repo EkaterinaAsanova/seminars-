42900ec524dc3e40b9909672efa8c755c11a5476
EkaterinaAsanova <u-kat@mail.ru>
2022-12-06 16:11:35 +0500

    По номеру четверти выдает диапазон возможных координат точек. Массив

---
+++ task3_6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 18. Напишите программу ,которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (Х и У)
+ // C применением массива! 
+
+string[] output = {"x>0, y>0" , "x<0, y>0" , "x<0, y<0" , "x>0, y<0"};
+int number  = Convert.ToInt32 (Console.ReadLine());
+// вoзможные четверти 1,2,3,4 
+// 0,1,2,3
+if (number <5 && number>0)
+Console.WriteLine(output[number-1]);
+else
+Console.WriteLine("input incorrect");
