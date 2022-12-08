d2919a95777c0435314e108c8ac400043184d05f
EkaterinaAsanova <u-kat@mail.ru>
2022-12-08 15:47:46 +0500

    Принимает число и выдает количество цифр в числе

---
+++ task4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №26 Напишите программу, которая принимает на вход число и выдает количество цифр в числе. 
+// 456 = 3
+// 78 = 2
+// 876544 = 6
+
+Console.WriteLine("Введите число");
+ int number = Convert.ToInt32(Console.ReadLine());
+ int count = 0;
+ while (number > 0)
+ {
+number/=10;
+count ++;    
+ }
+ Console.WriteLine("Количество цифр:" +count);
