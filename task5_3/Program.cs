2167f02a4dc40c6a07d4b275619c83d8e442c37a
EkaterinaAsanova <u-kat@mail.ru>
2022-12-09 12:59:54 +0500

    Поиск заданного числа в массиве

---
+++ task5_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача №33 Задайте массив. Напишите программу, которая определяет присутствует ли заданное число в массиве.
+// Число 4. [3,5,6,-1] - нет
+// Число 5. [3,5,6,-1] - да
+
+Console.WriteLine("Введите искомое число");
+ int n = Convert.ToInt32(Console.ReadLine());
+Random random = new Random();//Создали экземпляр класса Random, чтобы генерировать случайные числа
+int[] array = new int[12]; //Создали массив на 12 элементов с именем array
+for (int i = 0; i < array.Length; i++)//Создали цикл, чтобы пройтись по каждому элементу array
+{
+    array[i] = random.Next(-10,11);//Заполняем одну ячейку array случайным числом от -9 до 9. 
+}
+Console.WriteLine(string.Join(",", array));
+Console.WriteLine(FindElement(array,n));
+
+bool FindElement (int [] array, int n)
+{
+    bool count=false;
+    for (int i = 0; i < array.Length; i++)
+    if (array[i]==n)
+    count=true; 
+    return count;
+}
+
